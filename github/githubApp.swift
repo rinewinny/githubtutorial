d1ddd7cb9c244d8c41c42bcd36a11ee6d8480db4
anne <annelephamm@gmail.com>
2024-07-12 11:51:02 -0700

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Anne Pham on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
